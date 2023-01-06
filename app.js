5a624dfc8efbccb535b2b9a413bfb2514a93c543
Musa Ejaz <musa.ejaz@kpmg.co.uk>
2023-01-06 10:53:19 +0000

    'added seed api'

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const { patchVotesInArticle, getArticles, getArticleById, getArticleComments, po
 
 const app = express();
 
-const {handle404Paths, handleSql, handleCustomerErrors, handle500s} = require('./error.controller')
-
+const {handle404Paths, handleSql, handleCustomerErrors, handle500s} = require('./error.controller');
+const seed = require('./db/seeds/seed');
+const devData = require('./db/data/development-data')
 app.use(express.json())
 
 app.get('/api/topics', getTopics);
@@ -14,6 +15,11 @@ app.get('/api/articles/:article_id', getArticleById) ;
 app.get('/api/articles/:article_id/comments', getArticleComments)
 app.post('/api/articles/:article_id/comments', postCommentInArticle)
 app.patch('/api/articles/:article_id', patchVotesInArticle)
+app.get('/api/seed', (req, res, next) => {
+     seed(devData).then(() => {
+        res.send({msg:'seeded okay'})
+     });
+})
 
 app.all('*', handle404Paths);
 app.use(handleCustomerErrors)
